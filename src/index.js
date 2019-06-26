954d114a4c6a64528ad897bba0add62e7f5bc8c5
Agus <agustinmodugno@gmail.com>
2019-06-26 10:54:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as utils from './utils';
 
 // get from whatever version of react native that is being used.
-const AsyncStorage = (require('react-native') || {}).AsyncStorage;
+const AsyncStorage = (require('@react-native-community/async-storage') || {}).AsyncStorage;
 
 const storage = {
   setItem(key, value) {
